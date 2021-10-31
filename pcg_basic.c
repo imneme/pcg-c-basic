8d61b8b022eb8812610b7258cf26d403b6a874e7
Andy Huang <huang-a@users.noreply.github.com>
2021-10-31 18:10:05 -0400

    Change pcg_basic.* to match tul implementation

--- pcg_basic.c
+++ pcg_basic.c
@@ -28,13 +28,8 @@
  * your project.
  */
 
-#include "pcg_basic.h"
+#include <pcg_prng/pcg_basic.h>
 
-// state for global RNGs
-
-static pcg32_random_t pcg32_global = PCG32_INITIALIZER;
-
-// pcg32_srandom(initstate, initseq)
 // pcg32_srandom_r(rng, initstate, initseq):
 //     Seed the rng.  Specified in two parts, state initializer and a
 //     sequence selection constant (a.k.a. stream id)
@@ -48,12 +43,6 @@ void pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate, uint64_t initseq)
     pcg32_random_r(rng);
 }
 
-void pcg32_srandom(uint64_t seed, uint64_t seq)
-{
-    pcg32_srandom_r(&pcg32_global, seed, seq);
-}
-
-// pcg32_random()
 // pcg32_random_r(rng)
 //     Generate a uniformly distributed 32-bit random number
 
@@ -66,13 +55,7 @@ uint32_t pcg32_random_r(pcg32_random_t* rng)
     return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
 
-uint32_t pcg32_random()
-{
-    return pcg32_random_r(&pcg32_global);
-}
 
-
-// pcg32_boundedrand(bound):
 // pcg32_boundedrand_r(rng, bound):
 //     Generate a uniformly distributed number, r, where 0 <= r < bound
 
@@ -106,11 +89,5 @@ uint32_t pcg32_boundedrand_r(pcg32_random_t* rng, uint32_t bound)
         if (r >= threshold)
             return r % bound;
     }
-}
-
-
-uint32_t pcg32_boundedrand(uint32_t bound)
-{
-    return pcg32_boundedrand_r(&pcg32_global, bound);
 }
 
